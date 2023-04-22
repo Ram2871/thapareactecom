222e497b2fc411cc93b21f72d14fd57270f928b8
Ram2871 <ramchandra@r2logics.com>
2023-04-22 18:57:48 +0530

    ram

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Home';
-import About from './About';
-import Contact from './Contact';
-import Products from './Products';
-import Cart from './Cart';
-import SingleProduct from './SingleProduct';
-import Error from './Error';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import Products from "./Products";
+import Contact from "./Contact";
+import Cart from "./Cart";
+import SingleProduct from "./SingleProduct";
+import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import Header from './components/Header';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   const theme = {
@@ -20,9 +20,9 @@ const App = () => {
       white: "#fff",
       black: " #212529",
       helper: "#8490ff",
+      logo: '#CF998F',
 
       bg: "#F6F8FA",
-      header_bg: '#DCA1A1',
       footer_bg: "#0a1435",
       btn: "rgb(98 84 243)",
       border: "rgba(98, 84, 243, 0.5)",
@@ -37,22 +37,24 @@ const App = () => {
       mobile: "768px",
       tab: "998px",
     },
-  }
+  };
+
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <Router>
         <GlobalStyle />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/singleproduct/:id" element={<SingleProduct />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/single_product/:id" element={<SingleProduct />} />
-          <Route path="*" element={<Error />} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
-      </BrowserRouter>
+        <Footer />
+      </Router>
     </ThemeProvider>
   );
 };
